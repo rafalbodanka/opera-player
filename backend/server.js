ed089a00576cdb52618acaf16c4f21eb1fc53682
Rafał - laptop <rafal.bodanka@gmail.com>
2023-11-09 20:13:04 +0100

    fix econnaborted error for audio fetching

--- backend/server.js
+++ backend/server.js
@@ -13,53 +13,63 @@ const __dirname = path.dirname(__filename);
 
 const mediaDirectory = path.join(__dirname, 'media');
 
-app.get('/get-audio/:id', (req, res) => {
+app.get('/get-audio/:id', async (req, res) => {
     const { id } = req.params;
-    const audioFilePath = path.join(mediaDirectory, 'audio', `${id}.mp3`);
-
     if (id && /^\d+$/.test(id) && id >= 0 && id <= 9) {
-        res.sendFile(audioFilePath, (err) => {
-            if (err) {
-                res.status(404).send('Audio file not found');
-                return
+        const audioFilePath = path.join(mediaDirectory, 'audio', `${id}.mp3`);
+        
+        // Use try-catch to handle any errors when sending the file
+        try {
+            res.sendFile(audioFilePath, (err) => {
+                if (err) {
+                    if (!res.headersSent) {
+                        // Check if headers have been sent, and if not, send an error response
+                        console.log(err);
+                        return res.status(404).send('Audio file not found');
+                    }
+                }
+            });
+        } catch (err) {
+            console.log(err);
+            if (!res.headersSent) {
+                // Check if headers have been sent, and if not, send an error response
+                return res.status(500).send('Internal Server Error');
             }
-            return
-        });
+        }
     } else {
-        res.status(404).send('Invalid ID');
-        return
+        return res.status(404).send('Invalid ID');
     }
 });
 
-app.get('/get-image/:id', (req, res) => {
+app.get('/get-image/:id', async (req, res) => {
     const { id } = req.params;
     const imageFilePath = path.join(mediaDirectory, 'images', `${id}.png`);
 
     if (id && /^\d+$/.test(id) && id >= 0 && id <= 9) {
-        res.sendFile(imageFilePath, (err) => {
+        return res.sendFile(imageFilePath, (err) => {
             if (err) {
-                res.status(404).send('Image file not found');
-                return
+                return res.status(404).send('Image file not found');
             }
             return
         });
     } else {
-        res.status(404).send('Invalid ID');
-        return
+        return res.status(404).send('Invalid ID');
     }
 });
 
 app.get('/data', async (req, res) => {
     const dataPath = path.join(__dirname, 'media','data.json');
     
-    try {
-        const data = await fs.readFile(dataPath, 'utf-8');
-        res.json(JSON.parse(data));
-    } catch (error) {
+    await fs.readFile(dataPath, 'utf-8')
+      .then((data) => {
+        return res.json(JSON.parse(data));
+      })
+      .catch((error) => {
         console.error(error);
-        res.status(500).send('Internal Server Error');
-    }
-});
+        return res.status(500).send('Internal Server Error');
+      });
+      return
+  });
 
 
 const port = 5000;
